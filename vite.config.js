0171d0ae0f2b1aa5faca2b44a6d9b270864ee926
incognitomod <incognitomod22@gmail.com>
2025-09-19 23:02:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHub Pages requires the `base` path set to your repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/rampurhat-one-web/', // <-- your GitHub repo name
   server: { host: true }
 })
