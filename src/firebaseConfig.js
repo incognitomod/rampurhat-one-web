1207f8b24d509a236588ae3562814ab7af4f296d
incognitomod <incognitomod22@gmail.com>
2025-09-19 20:27:45 +0530

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+// REPLACE these placeholders with your Firebase project config (Step 2 will show how)
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+}
